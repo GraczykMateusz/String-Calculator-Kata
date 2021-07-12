e588fcb5371d2a31f88eec71a570d51861439f36
GraczykMateusz <43554417+GraczykMateusz@users.noreply.github.com>
2021-07-12 11:16:04 +0200

    {GREEN} empty string as add method arg should return 0

--- src/main/java/pl/graczykmateusz/StringCalculatorKata/StringCalculator.java
+++ src/main/java/pl/graczykmateusz/StringCalculatorKata/StringCalculator.java
@@ -4,6 +4,8 @@ public class StringCalculator implements IStringCalculator {
 
     @Override
     public int add(String numbers) {
+        if (numbers.isEmpty())
+            return 0;
         return 1;
     }
 }
